cf06cbbdbaa97d217003c42a213192d37fc660eb
Nef <nefesmeralda@gmail.com>
2021-08-28 08:14:40 -0500

    Updated parameters

--- main.cpp
+++ main.cpp
@@ -565,7 +565,7 @@ void UiInit()
 	shieldPrizeCoinsR.y = shieldPriceText.dstR.y;
 
 	shotgunPriceText = shieldPriceText;
-	shotgunPriceText.setText(renderer, robotoF, 20);
+	shotgunPriceText.setText(renderer, robotoF, 100);
 
 	shotgunText = shieldText;
 	shotgunText.setText(renderer, robotoF, "Shotgun");
