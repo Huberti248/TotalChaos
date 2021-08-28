e2c49767aa294e3c4f259060bf6f14fc3cb41450
ajyang2000 <alexander_yang_12@hotmail.com>
2021-08-28 03:16:08 -0400

    Added Controls Menu and Increased PLayer Size.

--- Entity.h
+++ Entity.h
@@ -3,13 +3,19 @@
 
 #include "SpawnPlace.h"
 
+enum class PlanetType {
+    Shield,
+    Weapon,
+};
+
 //Parent entity class
 class Entity {
 public:
-	SDL_FRect r{};
-	float dx = 0;
-	float dy = 0;
+    SDL_FRect r{};
+    float dx = 0;
+    float dy = 0;
+    PlanetType planetType = PlanetType::Shield;
 
-	Entity();
-	~Entity();
+    Entity();
+    ~Entity();
 };
